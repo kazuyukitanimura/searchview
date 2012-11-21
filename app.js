32c4b38a098bc6036e5cb566e5193cad5a451e47
kazuyukitanimura <kazuyukitanimura@gmail.com>
2012-11-20 21:23:17 -0800

    robots.txt humans.txt

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var search = require('./routes/search');
+var others = require('./routes/others');
 var http = require('http');
 var path = require('path');
 
@@ -32,6 +33,8 @@ app.configure('development', function() {
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/search', search.index);
+app.get('/robots.txt', others.robots);
+app.get('/humans.txt', others.humans);
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
