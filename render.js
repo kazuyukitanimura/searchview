e33552e75fb55c70c499151ab575147782f6cde8
kazuyukitanimura <kazuyukitanimura@gmail.com>
2012-12-01 00:17:12 -0800

    node.js example

--- render.js
+++ render.js
@@ -3,18 +3,18 @@ var fs = require('fs');
 
 new WebKit.Browser(function() {
   //this.setHtml('<!DOCTYPE html><html><head></head><body><h1>Hello, World!</h1></body></html>', function(error) {
-  this.visit('http://www.google.com', function(error) {
+  this.visit('http://www.nodejs.org', function(error) {
     if (error) {
       console.error(error);
       this.stop();
     } else {
-      this.highlightRender(800, 600, 'google search', function(error, data) {
+      this.highlightRender(985, 'node event-driven', function(error, data) {
         if (error) {
           console.error(error);
         } else {
           var self = this;
           var decodedImage = new Buffer(data, 'base64');
-          fs.writeFile('test.png', decodedImage, function(error) {
+          fs.writeFile('test0.png', decodedImage, function(error) {
             if (error) {
               console.error(error);
             }
