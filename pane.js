708601e3a402e7a0cf7501f9fe2f489241d0e1b7
kazuyukitanimura <kazuyukitanimura@gmail.com>
2012-12-30 23:46:48 -0800

    pane.js

---
+++ pane.js
@@ -0,0 +1,19 @@
+var Pane = require('Pane');
+
+var opt = {
+  title: 'Test',
+  height: 500,
+  width: 800,
+  url: 'http://nodejs.org'
+};
+
+var window = new Pane(opt);
+
+window.on('console', function(msg, line, src) {
+  console.log(msg, line, src);
+});
+
+window.on('loaded', function(succ) {
+  window.screenshot(__dirname + '/test.png');
+});
+
